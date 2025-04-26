f0bb8f26b6778b8d2c70db4d6a18d7e78654dbd6
David Oluwasegun Moses <mosesdave97372@yahoo.com>
2025-04-26 06:10:51 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
+// import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Set up Pug as the templating engine
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('pug');
+  // app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  // app.setViewEngine('pug');
 
   // Use the port assigned by the hosting environment or default to 3000
   const port = process.env.PORT || 3000;
